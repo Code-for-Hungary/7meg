47bb9913b8da6990fc9e24900a59d44732e16581
gazdagergo <info@gazdagergo.com>
2023-06-26 14:00:34 +0200

    felsooktatas

--- app/page.js
+++ app/page.js
@@ -229,6 +229,34 @@ export default function Home() {
                 <Counter {...calculateIntervalAndAmount(254 * MILLIARD, YEAR)} />
               </h5>
             </div>
+
+            <div className="relative flex flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a
+                className="absolute right-4 top-2 text-gray-500 hover:text-blue-600 cursor-pointer"
+                onClick={() => setdrawerData({
+                  title: 'Felsőoktatás',
+                  body: (
+                    <p>
+                      A felsőoktatásra fordított összeg a 2023-as évben 609,5 milliárd forint.
+                      <br /> <br /> Forrás: <a className="inline-flex items-center text-blue-600 hover:underline" href="https://koltsegvetes.k-monitor.hu/" target="_blank" rel="noreferrer">K-Monitor | átlátható költségvetés</a>
+                    </p>
+                  )
+                })}
+              >
+                <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mt-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+              </a>
+              <div className="pb-3">
+                <img src="/icons8-literature.gif" alt="Book" className="w-16 h-16 dark:invert" />
+              </div>
+              <p className="mb-3 font-normal text-gray-500 dark:text-gray-200">
+                Ennyi pénzt költöttünk el a felsőoktatás finanszírozására
+              </p>
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                <Counter {...calculateIntervalAndAmount(609.5 * MILLIARD, YEAR)} />
+              </h5>
+            </div>
+
+
           </div>
         </div>
       </main>
