505b69bdaea2fa92902cdb162757c288544490ae
gazdagergo <info@gazdagergo.com>
2023-10-08 15:17:56 +0200

    new favicon

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,17 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Tudta-e?',
+  title: 'Hét meg a nyolcát!',
   description: 'Milliárdok csepegtetve',
+  favicon: "/favicon-16x16.png"
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   )
 }
